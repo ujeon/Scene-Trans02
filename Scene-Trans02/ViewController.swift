3963aa679d1ce0c22f43a128a1dbb011fd430e44
ujeon <wj9304@gmail.com>
2021-07-22 17:30:01 +0900

    Initial Commit

---
+++ Scene-Trans02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-Trans02
+//
+//  Created by 우전 on 2021/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
