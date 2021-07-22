786717b7c56c328372b8ad99a8f614f3619a4383
ujeon <wj9304@gmail.com>
2021-07-22 21:41:32 +0900

    네비게이션 컨트롤러를 이용한 화면 이동

--- Scene-Trans02/ViewController.swift
+++ Scene-Trans02/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func moveByNavi(_ sender: Any) {
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") else {
+            return
+        }
+        
+        self.navigationController?.pushViewController(uvc, animated: true)
+    }
+    
+    @IBAction func movePresent(_ sender: Any) {
+        guard let uvc = self.storyboard?.instantiateViewController(withIdentifier: "SecondVC") else {
+            return
+        }
+        
+        self.present(uvc, animated: true)
+    }
 }
 
